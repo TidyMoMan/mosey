8341bc5cdf74afe3ad1fd62190e0e8b88137433f
TidyMoMan <mosesdilts@gmail.com>
2026-06-06 14:34:05 -0400

    test

---
+++ build/cpp_joystick/CMakeFiles/joystick_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joystick_publisher.dir/link.d"
+  "CMakeFiles/joystick_publisher.dir/src/joystick_publisher.cpp.o"
+  "CMakeFiles/joystick_publisher.dir/src/joystick_publisher.cpp.o.d"
+  "joystick_publisher"
+  "joystick_publisher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joystick_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
